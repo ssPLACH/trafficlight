20a7fb9d2928bc4960ef4cb12906dfbcca4b2c10
ssPLACH <134180679+ssPLACH@users.noreply.github.com>
2023-10-21 05:22:37 +0000

    estilo objeto basicolisto

---
+++ src/js/component/light.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Light = () => {
+    return (
+        <>
+            <div className="bg-dark p-2 light">
+              <button type="radio" className="btn btn-danger">danger</button>
+            </div>
+        </>
+    );
+};
+
+
+
+
+
+export default Light;
